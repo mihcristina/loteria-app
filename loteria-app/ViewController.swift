836e6914350eb7ad850f83c59453c0fcf2a1e7d0
Michelli <contato@michellicristina.com.br>
2021-12-31 21:17:05 -0300

    chore: configura app + adiciona imagens

---
+++ loteria-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loteria-app
+//
+//  Created by Michelli Cristina de Paulo Lima on 25/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
